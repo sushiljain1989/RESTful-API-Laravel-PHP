159f4f00018d630566b64dc0d51f1942fbb3bd54
Sushil jain <jain.ayushmaan@gmail.com>
2017-07-20 12:13:37 +0200

    curl examples in web.php for each http methods

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,15 @@
 |
 */
 
+#curl http://localhost/api/user/1
 Route::get('api/user/{id}', 'Users@getUser');
+
+#curl -H "Content-Type: application/json" -X POST -d '{"name":"Sushil Jain"}' \
+#http://localhost/api/create-user
 Route::post('api/create-user', 'Users@createUser');
+
+#curl -X PUT -d id=v1 -d name="rinky"
 Route::put('api/update-name', 'Users@updateName');
+
+#curl -X DELETE -d "id:1" http://localhost/api/delete-user
 Route::delete('api/delete-user', 'Users@deleteUser');
