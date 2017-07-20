b39601a4e49c678c145f54f8465df548d42812d3
Sushil jain <jain.ayushmaan@gmail.com>
2017-07-20 12:03:00 +0200

    added comments

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -9,17 +9,28 @@ class Users extends Controller
     //
 
 
-    /*Route::get('user/{id}', 'Users@getUser');
-    Route::post('create-user', 'Users@createUser');
-    Route::put('update-name', 'Users@updateName');
-    Route::delete('delete-user', 'Users@deleteUser'); */
+    /**
+     * @author [Sushil Jain]
+     * This function returs the Name and ID of a user
+     * @param  [int] $id [ID of the user]
+     * @return [array]     [returns the name and ID of the request user]
+     * @lastModifiedBy [Sushil Jain]
+     *
+     */
     function getUser($id)
     {
       //fetch user details recognized by ID, e.g. SSN
-      return ['name'=>"Sushil Jain"]; //return person name
+      return ["Used ID" => $id,'name'=>"Sushil Jain"]; //return person name
 
     }
 
+    /**
+     * @author [Sushil Jain]
+     * [This function creates a new user and values are received through http POST method]
+     * @param  Request $request [HTTP request object, in order to get values for creating a user]
+     * @return [array]           [success message]
+     * @lastModifiedBy [Sushil Jain]
+     */
     function createUser(Request $request)
     {
       //create a new user, user must have name
@@ -28,8 +39,13 @@ class Users extends Controller
 
     }
 
-
-
+    /**
+     * @author [Sushil Jain]
+     * [updateName this function updates the name of a user identified by ID]
+     * @param  Request $request [HTTP request object, it holds ID and new name for user]
+     * @return [array]           [success message]
+     * @lastModifiedBy [Sushil Jain]
+     */
     function updateName(Request $request)
     {
       //update person name, if person exists
@@ -37,6 +53,14 @@ class Users extends Controller
 
     }
 
+    /**
+     * @author [Sushil Jain]
+     * [deleteUser - this function deletes the user identified by ID]
+     * @param  Request $request [HTTP request object, it holds ID of a user]
+     * @return [array]           [success message]
+     * @lastModifiedBy [Sushil Jain]
+     *
+     */
     function deleteUser(Request $request)
     {
       //delete person recognized by ID
